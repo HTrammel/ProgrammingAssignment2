27a49f769041b218bff8c81db56c4fa255cc3c43
HTrammel <harold.trammel@aspca.org>
2015-03-18 16:04:44 -0500

    Submission for evaluation
    Code has been tested using sample matrices from discussion groups.
    Style note: I prefer clarity over compactness.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#-----------------------------------------------------
+## cachematrix.R
+## Author: Harold Trammel
+## Purpose: These two functions cache and provide the inverse of a matrix for future use.  
+## Usage:  The "makeCacheMatrix" function must be called first, but 
+##         the cacheSolve is used for subsequent inversions.  
+#-----------------------------------------------------
 
-## Write a short comment describing this function
-
+## Stores a copy of the inverse of matrix "x". Used by cacheSolve function/
 makeCacheMatrix <- function(x = matrix()) {
-
+    cached_inversion <- NULL  
+    set <- function(y) {
+        x <<- y           
+        cached_inversion <<- NULL  
+    }
+    get <- function() {
+        return (x)       
+    }
+    setInverse <- function(new_inversion) {
+        cached_inversion <<- new_inversion
+    }
+    getInverse <- function() {
+        return (cached_inversion)
+    }
+    list( set = set
+          , get = get
+          , setInverse = setInverse
+          , getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'.  If the inverse of 'x'
+## has been stored return that, otherwise invert 'x' and cache it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cached_inverse <- x$getInverse()
+    
+    if (!is.null(cached_inverse)) {
+        # if 
+        message ("getting cached data...")
+        return (cached_inverse)
+    } else {
+        data <- x$get()
+        cached_inverse <- solve(data)
+        x$setInverse(cached_inverse)
+        message ("setting cached data...")
+        return(cached_inverse)
+    }
 }
